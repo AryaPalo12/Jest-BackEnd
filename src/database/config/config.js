67022bf181dd30a22011531ba5e4d243529a8885
suryo <putrantosuryo@gmail.com>
2022-09-14 15:54:50 +0700

    adding Database to Heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 
 const config = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,    
-    dialect: "postgres",
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // host : process.env.DB_HOST,
+    // port : process.env.DB_PORT,    
+    // dialect: "postgres",
+    url : process.env.DB_URL,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    },
     
   },
   test: {
