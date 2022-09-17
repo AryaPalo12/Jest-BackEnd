9c61859aee32ccec85fb00c6e3302d10185b2e04
abahuus <abahuus.1979@gmail.com>
2022-09-17 08:27:00 +0000

    Update user.repo.js

--- src/user/user.repo.js
+++ src/user/user.repo.js
@@ -1,1 +1,54 @@
+  const { User } = require("../database/models");
 
+  const getAllUser = async () => {
+    return await User.findAll();
+  };
+
+  const createUser = async ({ fullname, email, password }) => {
+    return await User.create({
+      fullname,
+      email,
+      password,
+    });
+  };
+
+  const checkEmailAllUser = async (email) => {
+    return await User.findOne({
+      where: { email: email },
+    });
+  };
+
+  const editUser = async ({ fullname, email, password, userId }) => {
+    return await User.update(
+      {
+        fullname,
+        email,
+        password,
+      },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+  };
+
+  const checkSameEmail = async ({email, authUserId}) => {
+    return await User.findOne({
+      where:{
+        email : email,
+        id : authUserId
+
+      }
+    }
+    )
+  }
+  const userRepo = {
+    createUser,
+    getAllUser,
+    editUser,
+    checkEmailAllUser,
+    checkSameEmail
+  };
+
+  module.exports = userRepo;
