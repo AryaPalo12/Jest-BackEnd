4cf9359430284e5ca297bc8e0fed8e816c020516
suryo <putrantosuryo@gmail.com>
2022-09-17 16:11:02 +0700

    setup deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -26,17 +26,17 @@ const config = {
     "dialect": "mysql"
   },
   production: {
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME,
-    // host : process.env.DB_HOST,
-    // port : process.env.DB_PORT,    
+    username: "fjgmwfgkgoldux",
+    password: "76999dc6829bbbfb7e06ae74d90ae03eeee7d211d99fde9b565df114e6a3381b",
+    database: "d3qcl26i0puh5q",
+    host : "ec2-52-207-90-231.compute-1.amazonaws.com",
+    port : "5432",    
     dialect: "postgres",
     url : process.env.DB_URL,
     dialectOptions: {
       ssl: {
-        require: true, // This will help you. But you will see nwe error
-        rejectUnauthorized: false // This line will fix new error
+        require: true,
+        rejectUnauthorized: false
       }
     },
   }
