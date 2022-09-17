9d92cb5103f52c5239ac448c965c6e87b145feac
suryo <putrantosuryo@gmail.com>
2022-09-17 16:05:35 +0700

    deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 
 const config = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,    
-    dialect: "postgres",
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // host : process.env.DB_HOST,
+    // port : process.env.DB_PORT,    
+    // dialect: "postgres",
     url : process.env.DB_URL,
     dialectOptions: {
       ssl: {
@@ -26,11 +26,11 @@ const config = {
     "dialect": "mysql"
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,    
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // host : process.env.DB_HOST,
+    // port : process.env.DB_PORT,    
     dialect: "postgres",
     url : process.env.DB_URL,
     dialectOptions: {
