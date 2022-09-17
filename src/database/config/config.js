d72cf97bba4a9757e76155402cec10fa9bb7734b
suryo <putrantosuryo@gmail.com>
2022-09-17 14:30:11 +0700

    setup DB for deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,7 +9,6 @@ const config = {
     host : process.env.DB_HOST,
     port : process.env.DB_PORT,    
     dialect: "postgres",
-    url : process.env.DB_URL,
     dialectOptions: {
       ssl: {
         require: true, // This will help you. But you will see nwe error
@@ -18,19 +17,8 @@ const config = {
     },
     
   },
-  test: {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,    
+
+  production: {   
     dialect: "postgres",
     url : process.env.DB_URL,
     dialectOptions: {
