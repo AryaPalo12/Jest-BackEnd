ae038ca8a21136fdb463cfb20563fb766be57e46
greiq <jjanuar286@gmail.com>
2022-09-17 14:47:11 +0700

    test

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -6,6 +6,7 @@ const { validate } = require("../middleware/validation");
 const userRouter = express.Router();
 
 const userController = require("./user.controller");
+
 // API GET ALL USER
 
 /**
@@ -39,101 +40,7 @@ const userController = require("./user.controller");
 
 //---------------------------------------------------------------------#
 
-userRouter.get("/users", userController.getAllUser)
-
-// API USER REGISTRATION
-/**
- * @swagger
- * /user/registration:
- *  post:
- *    tags:
- *      - users
- *    summary: API User Registration (PUBLIC & VALIDATION)
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            type: object
- *            properties:
- *              fullname:
- *                type: string
- *                example: Greiq Imapuly
- *              email:
- *                type: string
- *                example: greiq@gmail.com
- *              password:
- *                type: string
- *                example: 123456&Qz
- *    responses:
- *      '200':
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                id:
- *                  type: string
- *                fullname:
- *                  type: string
- *                email:
- *                  type: string
- *                password:
- *                  type: string
- *                updatedAt:
- *                  type: string
- *                createdAt:
- *                  type: string
- *     
- */
-  
-
-//---------------------------------------------------------------------#
-
-userRouter.post("/user/registration",checkSchema(registrationValidationObject),
-validate, userController.createUser);
-
-// API EDIT USER 
-
-/**
- * @swagger
- * /user/{userId}:
- *  put:
- *    security:
- *      - bearerAuth : []
- *    tags:
- *      - users
- *    summary: API Edit User (PRIVATE & VALIDATION)
- *    parameters:
- *      - in: path
- *        name: userId
- *        value : 1
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            type: object
- *            properties:
- *              fullname:
- *                type: string
- *                example: Nama lengkap
- *              email:
- *                type: string
- *                example: contoh@gmail.com
- *              password:
- *                type: string
- *                example: Password@123!
- *    responses:
- *      '200':
- *        description: Edit data sukses
- */
-
-
-userRouter.put("/user/:userId",tokenVerification, checkSchema(registrationValidationObject),
-validate, userController.editUser)
-
-//---------------------------------------------------------------------#
+// userRouter.get("/users", userController.getAllUser)
 
 
 
--- src/user/user.service.js
+++ src/user/user.service.js
@@ -3,49 +3,8 @@ const tokenVerification = require("../middleware/token.verification");
 const userRepo = require("./user.repo");
 const saltRound = 10;
 
-const getAllUser = async () => {
-   const getAllUserinRepo = await userRepo.getAllUser();
-   return getAllUserinRepo;
-}
 
-
-const createUser = async ({ fullname, email, password }) => {
-  const hashPassword = await bcrypt.hash(password, saltRound);
-
-  const checkEmailUser = await userRepo.checkEmailAllUser(email);
-
-  if(!checkEmailUser){
-  const getUserRepo = await userRepo.createUser({ fullname, email, password: hashPassword });
-  return getUserRepo;
-  }
-  else return null;
-};
-
-const editUser = async({  fullname, email, password, userId, authUserId}) =>{
-
-  const hashedPassword = await bcrypt.hash(password, saltRound)
-
-  //CEK EMAIL YANG INGIN DIUPDATE ADA ATAU ENGGA DALAM TABEL USERS
-  const checkEmailAllUser = await userRepo.checkEmailAllUser(email);
-
-  //Jika hanya ingin update Fullnamenya/passwordnya aja
-  const checkSameEmail = await userRepo.checkSameEmail({email, authUserId});
-
-  if(!checkEmailAllUser || checkSameEmail){
-      const getUserRepo = await userRepo.editUser({
-      fullname, 
-      email,
-      password : hashedPassword, 
-      userId
-    })
-    return getUserRepo;
-  }
-  else return null;
-}
 const userService = {
-  createUser,
-  getAllUser,
-  editUser 
 };
 
 module.exports = userService;
