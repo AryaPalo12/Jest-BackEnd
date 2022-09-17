cd4f4668891abad3070cacebea3189c37612d705
suryo <putrantosuryo@gmail.com>
2022-09-17 16:15:19 +0700

    deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -26,11 +26,11 @@ const config = {
     "dialect": "mysql"
   },
   production: {
-    username: "fjgmwfgkgoldux",
-    password: "76999dc6829bbbfb7e06ae74d90ae03eeee7d211d99fde9b565df114e6a3381b",
-    database: "d3qcl26i0puh5q",
-    host : "ec2-52-207-90-231.compute-1.amazonaws.com",
-    port : "5432",    
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,    
     dialect: "postgres",
     url : process.env.DB_URL,
     dialectOptions: {
